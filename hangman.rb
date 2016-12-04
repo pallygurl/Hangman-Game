ab108a5a8d330a6eeb76ad70fc3eb0169c3a5a97
Cyndi Bolton <cbolton@wvtpg.com>
2016-12-04 10:54:28 -0500

    added new code for update tries; all tests passing

--- hangman.rb
+++ hangman.rb
@@ -1,45 +1,59 @@
 class Hangman
     attr_reader :word
-    attr_accessor :spaces, :guessed_letters, :tries
+    attr_accessor :correct_guesses, :guessed_letters, :tries
 
     def initialize(word)
          @word = word
-         @spaces = " " * word.length
+         @correct_guesses = " " * word.length
          @guessed_letters = []
          @tries = 6
     end
 
-    def word_include?(guess)  #this method determines if the guessed letter is in the word
-      word.include?(guess)
+    # def guess(letter)
+    #   letter.upcase!
+    #   unless @guessed_letters.include?(letter)
+    #     @guessed_letters << letter
+    #   end
+    #   if word.include?(letter)
+    #     @correct_guesses << letter
+    #   else
+    #     @tries -=1
+    #   end
+    # end
+
+    def word_include?(letter)  #this method determines if the guessed letter is in the word
+      word.include?(letter)
     end
 
-    def make_guess(guess) #this method determines that letters goes into the guessed letters array
-      @guessed_letters << guess
+    def make_guess(letter) #this method determines that letters go into the guessed letters array
+      @guessed_letters << letter
     end
 
-    def update_spaces(guess) #this method determines that letters that are part of the word are correctly updated
+    def update_correct_guesses(letter) #this method determines that letters that are part of the word are correctly updated
       i = 0 #sets a counter to 0
       word.length.times do #iterates through the word
-        if word[i] == guess #looks at the index value of the word & if the guessed letter is in the right spot
-          @spaces [i] = guess #makes the space at that index value equal to the guessed letter
+        if word[i] == letter #looks at the index value of the word & if the guessed letter is in the right spot
+          @correct_guesses [i] = letter #makes the space at that index value equal to the guessed letter
         end
         i += 1 #increments the counter
       end
 
     end
 
-    def update_tries #this method decrements the chances by 1
+    def update_tries(letter) #this method decrements the chances by 1
+      if word.include?(letter)
+      @tries
+      else
       @tries -= 1
+      end
     end
 
-    def already_guessed?(guess)
-      @guessed_letters.include?(guess)
+    def already_guessed?(letter) #this method looks to see if guessed letters are already in the guessed letters array
+      @guessed_letters.include?(letter)
     end
 
     def game_over?
-      word == @spaces || @tries == 0
-      #   true
-      # end
+      word == @correct_guesses || @tries == 0 #spaces holds the correct letters that form the word. when equal to the word, the game is over
     end
 
 end
--- test_hangman.rb
+++ test_hangman.rb
@@ -10,7 +10,7 @@ class TestHangman < Minitest::Test
 
 	def test_number_of_spaces
 		game = Hangman.new("beer")
-		assert_equal("    ", game.spaces)
+		assert_equal("    ", game.correct_guesses)
 	end
 
 	def test_if_guessed_letter_is_in_word
@@ -33,48 +33,48 @@ class TestHangman < Minitest::Test
 
 	def test_update_spaces
 		game = Hangman.new("beer")
-		game.update_spaces("b")
-		assert_equal("b   ", game.spaces)
+		game.update_correct_guesses("b")
+		assert_equal("b   ", game.correct_guesses)
 	end
 
 	def test_update_spaces_2
 		game = Hangman.new("beer")
-		game.update_spaces("e")
-		assert_equal(" ee ", game.spaces)
+		game.update_correct_guesses("e")
+		assert_equal(" ee ", game.correct_guesses)
 	end
 
 	def test_update_spaces_3
 		game = Hangman.new("beer")
-		game.update_spaces("r")
-		assert_equal("   r", game.spaces)
+		game.update_correct_guesses("r")
+		assert_equal("   r", game.correct_guesses)
 	end
 
-	# def test_number_of_tries_decreases_with_incorrect_guess
-	# 	game = Hangman.new("beer")
-	# 	guess = "j"
-	# 	game.tries = 4
-	# 	assert_equal(3, game.update_tries)
-	# end
-	#
-	# def test_number_of_tries_does_not_decrease_with_correct_guess
-	# 	game = Hangman.new("beer")
-	# 	guess = "b"
-	# 	game.tries = 5
-	# 	assert_equal(5, game.update_tries)
-	# end
+	def test_number_of_tries_decreases_with_incorrect_guess
+		game = Hangman.new("beer")
+		letter = "j"
+		game.tries = 4
+		assert_equal(3, game.update_tries(letter))
+	end
+
+	def test_number_of_tries_does_not_decrease_with_correct_guess
+		game = Hangman.new("beer")
+		letter = "b"
+		game.tries = 5
+		assert_equal(5, game.update_tries(letter))
+	end
 
 	def test_for_letter_already_been_guessed_is_true
 		game = Hangman.new("beer")
-		guess = "b"
+		letter = "b"
 		game.guessed_letters = ["b"]
-		assert_equal(true, game.already_guessed?(guess))
+		assert_equal(true, game.already_guessed?(letter))
 	end
 
 	def test_for_letter_already_been_guessed_is_false
 		game = Hangman.new("beer")
-		guess = "b"
+		letter = "b"
 		game.guessed_letters = ["q", "h", "e"]
-		assert_equal(false, game.already_guessed?(guess))
+		assert_equal(false, game.already_guessed?(letter))
 	end
 
 	def test_for_game_over
@@ -91,13 +91,13 @@ class TestHangman < Minitest::Test
 
 	def test_for_game_over
 		game = Hangman.new("beer")
-		game.spaces = "bee "
+		game.correct_guesses = "bee "
 		assert_equal(false, game.game_over?)
 	end
 
 	def test_for_game_over
 		game = Hangman.new("beer")
-		game.spaces = "beer"
+		game.correct_guesses = "beer"
 		assert_equal(true, game.game_over?)
 	end
 
